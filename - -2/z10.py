b0d4de8cde784b6ebca2b1383d30ab71157d007c
ArtemDud10K <dydik_10@mail.ru>
2017-06-19 17:08:32 +0300

    Delete z10.py

--- - -2/z10.py
+++
@@ -1,5 +0,0 @@
-import datetime
-date = input('Введите дату в формате гггг.дд.мм\n')
-date1 = datetime.datetime.date(datetime.datetime.strptime(date, '%Y.%d.%m')) #перевод даты в нужный формат
-date2 = datetime.timedelta(days=1)#приближение в один день
-print(date2 + date1)#ответ задачи
