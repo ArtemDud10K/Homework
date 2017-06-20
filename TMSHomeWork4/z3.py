642601a3537f3f70a6c90080b835649fab03259a
ArtemDud10K <dydik_10@mail.ru>
2017-06-20 22:42:11 +0300

    Add files via upload

--- TMSHomeWork4/z3.py
+++ TMSHomeWork4/z3.py
@@ -1,7 +1,7 @@
 import random
 def min_(l):
 	min1 = l[0]
-	for i in l:
+	for i in l[1::]:
 		if i < min1:
 			min1 = i
 	return min1
@@ -11,5 +11,6 @@ l = []
 for i in range(random.randint(5, 10)):
 	l.append(random.randint(0, 10))
 print(l)
+print(min(l))
 print(min_(l))
 			 
