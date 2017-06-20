9277b6a6efe1b16ab8be1b902bde23f78e4bf068
ArtemDud10K <dydik_10@mail.ru>
2017-06-20 22:48:54 +0300

    Update z28.py

--- TMSHomeWork4/z28.py
+++ TMSHomeWork4/z28.py
@@ -1,15 +1,22 @@
 def decor(func):
-	def transform(l, *args):
+	def transform(*args):
+		l = []
 		for i in args:
-			l.append(i)
+			if isinstance(i, list):
+				for j in  i:
+					l.append(j) 
+			else:
+				l.append(i)
 		return func(l)
 	return transform
 
+@decor
 def sum_(l):
 	summ = 0
 	for i in l: 
 		summ += i
 	print (f'Сумма: {summ}')
 
+
 sum_ = decor(sum_)
-print(sum_([1, 2, 3], 4, 5))
+print(sum_(1, [2, 3], 4, 5))
