b33da41727bb76b7c542579eda6e2645268cbdb2
ArtemDud10K <dydik_10@mail.ru>
2017-06-21 00:35:29 +0300

    Add files via upload

--- TMSHomeWork-3/z38.py
+++ TMSHomeWork-3/z38.py
@@ -1,13 +1,11 @@
 data = input('Введите фамилию, имя, возраст, рост и вес\n')
 list1 = data.split()
 for i in list1:
-	surname = str(list1.pop(0))
-	name = str(list1.pop(0))
-	age = int(list1.pop(0))
-	higth = float(list1.pop(0))
-	weight = float(list1.pop(0))
-print(f'Имя: {name} {type(name)}')   
-print(f'Фамилия: {surname} {type(surname)}')
-print(f'Возраст: {age} {type(age)}')
-print(f'Рост: {higth} {type(higth)}')
-print(f'Вес: {weight} {type(weight)}')
+	if i.isalpha():
+		i = str(i)
+	else:
+		if i.isdigit() and i.find('.') == -1:
+			i = int(i)
+		else:
+			i = float(i)
+	print((i, type(i)))
