685b2a10d311e8d0960bae35c6de7523c5e58bf8
ArtemDud10K <dydik_10@mail.ru>
2017-06-20 23:56:06 +0300

    Update z42.py

--- TMSHomeWork-3/z42.py
+++ TMSHomeWork-3/z42.py
@@ -1,10 +1,11 @@
 a = int(input('Введите число для извлечения квадратного корня\n'))
-x = 1
-eps = 0.000001
-y = 0
+x = 0.1
+eps = 0
 y1 = a ** 1/2
-m = 0
+iteration = 0
+y = x * 10
 while abs(y1 - y) > eps:
 	y = (x + a/2)/2
-	x += 1 
-print(y, m) 
+	x += 0.1
+	iteration += 1
+print(y, iteration)
