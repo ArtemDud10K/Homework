4870a53cf933874d22d3d58a4355fbcacc03e624
ArtemDud10K <dydik_10@mail.ru>
2017-06-20 22:34:23 +0300

    Add files via upload

--- TMSHomeWork4/z6.py
+++ TMSHomeWork4/z6.py
@@ -1,12 +1,12 @@
-def enumerate_ (seq):
-	idx = 0
-	for i in seq:
-		print(i)
-		i += 1
-	
-l = [1, 2, 3]
-for idx, item in enumerate(l):
-        print(idx)
+def enumerate_(sequence, start=0):
+    n = start
+    for elem in sequence:
+        yield n, elem
+        n += 1
 
-for idx, item in enumerate_(l):
-        print(idx)
+l = [1, 2, 3]
+for idx, i in enumerate(l):
+        print(idx, i)
+print('---------------')
+for idx, i in enumerate_(l):
+        print(idx, i)
