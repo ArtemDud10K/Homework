faa16a4e05ccd474f6472f40342c2bf25cf3eac1
ArtemDud10K <dydik_10@mail.ru>
2017-06-21 19:29:25 +0300

    Update z15.py

--- TMSHomeWork-3/z15.py
+++ TMSHomeWork-3/z15.py
@@ -1,7 +1,9 @@
-first_list = [1, 2, 3, 4, [5, 6, 7 , 8]] 
-list_second = first_list.pop()
-for i in list_second:
-		first_list.append(i)
-print(first_list)
-		
+first_list = [1, 2, [5, 6, 7 , 8], 3, 4] 
+for i in first_list:
+	if isinstance(i, list):
+		second_list = i
+		index = first_list.index(second_list)
+first_list.pop(index)
+first_list.extend(second_list)
+print(first_list)		
 
